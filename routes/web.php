50a5ce9bfed923f014aaa020ae37203df3dbbb93
José Raúl Botzoc Mérida <84558182+Raul-OXRI@users.noreply.github.com>
2026-03-27 09:24:15 -0600

    Refactor routes to use OnlineController for home

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OnlineController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/online', [OnlineController::class, 'index'])->name('online.index');
+
+Route::get('/', [OnlineController::class, 'index'])->name('online.index');
 Route::get('/online/pdf', [OnlineController::class, 'exportPDF'])->name('online.exportPDF');
 Route::get('/online/pdf-all', [OnlineController::class, 'exportPDFAll'])->name('online.exportPDFAll');
